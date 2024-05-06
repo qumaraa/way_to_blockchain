8eeb62466d6086b87ca976a0585420de3cee70b2
Alexander <112755279+qumaraa@users.noreply.github.com>
2024-05-06 15:06:28 +0400

    Update mempool.rs

--- #2_blockchain_impl/mempool.rs
+++ #2_blockchain_impl/mempool.rs
@@ -1,1 +1,7 @@
-// TODO
+use serde::{Deserialize, Serialize};
+use crate::{transaction::Transaction};
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Mempool {
+    transactions: Vec<Transaction>
+}
